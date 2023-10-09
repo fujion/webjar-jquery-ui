9124851beb10fc95c160df8cb38be78133a6a25d
Douglas Martin <mdgeek1@gmail.com>
2023-10-09 14:00:43 -0400

    Add post build validation.

---
+++ validate_.sh
@@ -0,0 +1,8 @@
+file_exists "import-map.json"
+file_contains "LICENSE.txt" "Copyright jQuery Foundation"
+
+dir_exists "dist/images"
+file_exists "dist/jquery-ui.css"
+file_exists "dist/jquery-ui.js"
+file_exists "dist/jquery-ui.structure.css"
+file_exists "dist/jquery-ui.theme.css"
