17e49279b0157d4ec0f43eb6e84432e42b01b3e1
Douglas Martin <mdgeek1@gmail.com>
2023-10-13 20:55:23 -0400

    Use gulp and rollup for builds.

---
+++ staging/gulpfile.js
@@ -0,0 +1,31 @@
+const {src, dest, series} = require('gulp');
+
+const srcDir = '${src.rootdir}/jquery-ui-${version.unrevise}/';
+const destDir = '${webjar.target}/';
+
+function task1() {
+    return _copy('LICENSE.txt');
+}
+
+function task2() {
+    return _copy(['*.css', '*.js'], 'dist');
+}
+
+function task3() {
+    return _copy('images/**', 'dist/images');
+}
+
+function _toSrc(_src) {
+    return src(_src, {allowEmpty: false, cwd: srcDir});
+}
+
+function _toDest(_dest) {
+    return dest(_dest || '.', {cwd: destDir});
+}
+
+function _copy(_src, _dest) {
+    console.log('  Copying ' + _src);
+    return _toSrc(_src).pipe(_toDest(_dest))
+}
+
+exports.default = series(task1, task2, task3);
